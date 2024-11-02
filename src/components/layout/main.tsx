dad80050bd69f1c2a72436429f9a049ebe116e3e
ionztorm <leon.codes@icloud.com>
2024-11-02 18:52:42 +0000

    add variants to section headers

--- src/components/layout/main.tsx
+++ src/components/layout/main.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/features/homepage/sections/hero/hero";
 import { Section } from "@/components/utility/section";
 import { About } from "@/features/homepage/sections/about/about";
+import { Skills } from "@/features/homepage/sections/skills/skills";
 
 export const Main = () => {
   return (
@@ -16,6 +17,9 @@ export const Main = () => {
         className="min-h-[100dvh] grid place-content-center bg-gray-50"
       >
         <About />
+      </Section>
+      <Section id="skills" className="min-h-[100dvh] grid place-content-center">
+        <Skills />
       </Section>
     </main>
   );
--- src/components/ui/section-heading.tsx
+++ src/components/ui/section-heading.tsx
@@ -1,16 +1,12 @@
-import { ChildrenProps } from "@/lib/types";
+import { SectionHeaderProps } from "@/lib/types";
 
-export const SectionHeader = ({ children }: ChildrenProps) => {
-  return (
-    <h2
-      className="
-          text-3xl md:text-5xl lg:text-6xl
-          font-extrabold
-          tracking-tighter
-        mb-5
-        "
-    >
-      {children}
-    </h2>
-  );
+export const SectionHeader = ({ children, variant }: SectionHeaderProps) => {
+  const variants = {
+    main: "text-3xl md:text-5xl lg:text-6xl font-extrabold tracking-tighter mb-5",
+    sub: "text-xl md:text-2xl lg:text-3xl font-medium",
+  };
+
+  const Component = variant === "main" ? "h2" : "h3";
+
+  return <Component className={variants[variant]}>{children}</Component>;
 };
