07301d3ce56c21057476d39f48808203982bea3f
ionztorm <leon.codes@icloud.com>
2024-11-06 15:23:34 +0000

    fix burger spacing

--- src/features/homepage/sections/navbar/components/burger-button.tsx
+++ src/features/homepage/sections/navbar/components/burger-button.tsx
@@ -13,7 +13,7 @@ export const BurgerButton = ({ isOpen, onClick }: BurgerButtonProps) => {
 				className={`h-0.5 w-6 bg-slate-50 transition-transform duration-300 ease-in-out before:absolute before:block before:h-0.5 before:w-6 before:bg-slate-50 before:transition-transform before:duration-300 before:ease-in-out before:content-[''] after:absolute after:block after:h-0.5 after:w-6 after:bg-slate-50 after:transition-transform after:duration-300 after:ease-in-out after:content-[''] ${
 					isOpen
 						? 'rotate-45 before:-translate-y-0 before:rotate-90 after:-translate-y-0 after:-rotate-90'
-						: 'before:-translate-y-[6px] after:translate-y-[7px]'
+						: 'before:-translate-y-[7px] after:translate-y-[7px]'
 				}`}
 			/>
 		</button>
